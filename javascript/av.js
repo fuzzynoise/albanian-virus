b837f7b0b9cd6bb3e0e7b92ba8870e8c4880a475
Mitch <watchthepuppetshow@gmail.com>
2020-06-23 13:25:29 -0400

    v0.1a

---
+++ javascript/av.js
@@ -0,0 +1,2 @@
+payload="Hi,\n\nI am an Albanian virus but because of poor technology in my country, unfortunately, I am not able to harm your computer.\n\nPlease be so kind to delete one of your important files yourself and then forward me to other users.\n\nMany thanks for your cooperation!\n\nBest regards,\nAlbanian virus";
+confirm(payload);
