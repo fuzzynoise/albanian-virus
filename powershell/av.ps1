c6383934b5f562304a3521fc546f90586fefa5e5
Mitch <watchthepuppetshow@gmail.com>
2020-06-27 23:12:00 -0400

    v0.1a

--- powershell/av.ps1
+++ powershell/av.ps1
@@ -1,1 +1,12 @@
-Write-Error 'oh no!' -TargetType write-style
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no, $cancel)
+$title = "VIRUS ALERTING"
+$payload = "Hi,
+
+I am an Albanian virus but because of poor technology in my country, unfortunately, I am not able to harm your computer.
+Please be so kind to delete one of your important files yourself and then forward me to other users.
+
+Many thanks for your cooperation!
+
+Best regards,
+Albanian virus"
+$result = $host.ui.PromptForChoice($title, $payload, $options, 1)
